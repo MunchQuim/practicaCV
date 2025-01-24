efe9a2b15af87300e560a3765160c98171e2e3b8
Quim <qpinedasotes@gmail.com>
2025-01-24 13:31:20 +0100

    ip https

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("http://munchquim.duckdns.org:2727/api2.php");
+        const res = await fetch("https://leon-scientific-biology-vot.trycloudflare.com/api2.php");
         
         const result = await res.json();  // Espera a que se resuelva la promesa y convierte la respuesta en JSON
         /* console.log(result.experienciasLaborales); */
