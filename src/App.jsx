5bddb6db5bc8b88f1e16ec09c495caca1dd3fe55
Quim <qpinedasotes@gmail.com>
2025-01-24 13:01:29 +0100

    ip para despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("172.17.131.75/api2.php");
+        const res = await fetch("http://munchquim.duckdns.org:2727/api2.php");
+        
         const result = await res.json();  // Espera a que se resuelva la promesa y convierte la respuesta en JSON
         /* console.log(result.experienciasLaborales); */
         setData(result);
