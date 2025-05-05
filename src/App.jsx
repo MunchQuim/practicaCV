faf15271f3a437fca8cfe4f9cbfa501561851ea7
munchQuim <qpinedasotes@gmail.com>
2025-05-05 15:49:32 +0200

    changed api domain

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("https://square-hookworm-sweeping.ngrok-free.app/api2.php");
+        const res = await fetch("https://munchquim.duckdns.org/api2.php");
         
         const result = await res.json();  // Espera a que se resuelva la promesa y convierte la respuesta en JSON
         /* console.log(result.experienciasLaborales); */
